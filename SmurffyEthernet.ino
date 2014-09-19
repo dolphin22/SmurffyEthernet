b0fac70346d840779ad96557c61bb912fe081a8d
Dolphin <nhancaheo.me@gmail.com>
2014-09-19 23:01:00 +0700

    Updated.

--- SmurffyEthernet.ino
+++ SmurffyEthernet.ino
@@ -7,25 +7,30 @@
 
 LibHumidity sht25 = LibHumidity(0);
 
-byte mac[] = {  
-  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
+byte mac[] = { 
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
   
 EthernetClient client;
 
-char serverName[] = "128.199.246.241";
-unsigned static int serverPort = 49156;
+IPAddress server(128,199,246,241); 
+//char serverName[] = "ismurffy.com";
+unsigned static int port = 49157;
+
+char deviceID[10] = "S001";
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   
   if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
+    Serial.println(F("DHCP failed"));
     for(;;) {
       ;
     }
   }
   
-  Serial.println("Connected to Router");
+  Serial.println(Ethernet.localIP());
+  
+  connectToServer();
   
   // Enable watchdog
   wdt_enable(WDTO_8S);
@@ -34,35 +39,62 @@ void setup() {
 void loop() {
   char tempF[5], humiF[5];
 
-  dtostrf(29.27,5,2,tempF);
-  dtostrf(81.29,5,2,humiF);
+  dtostrf(sht25.GetTemperatureC(),5,2,tempF);
+  dtostrf(sht25.GetHumidity(),5,2,humiF);
 
-  char data[255];
-  sprintf(data, "{\"temperature\": \"%.5s\", \"humidity\": \"%.5s\"}", tempF, humiF);
-  //Serial.println(F(data));
+  char data[80];
+  sprintf(data, "{\"deviceID\": \"%s\", \"temperature\": \"%.5s\", \"humidity\": \"%.5s\"}", deviceID, tempF, humiF);
+  Serial.println(data);
   
-  int length = 45;
-  //length = data.length();
-  
-  postDataToServer(length);
+  while (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  // if there's no net connection, but there was one last time
+  // through the loop, then stop the client:
+  if (client.connected()) {
+    sendData(data); 
+    
+  } else {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+    
+    connectToServer();
+  }
   
   // Reset watchdog
   wdt_reset();
   wdt_disable();
   
+  delay(10000);
+}
+
+void connectToServer()
+{
+  if (client.connect(server, port)) {
+    Serial.println(F("Server connected"));
+  } else {
+    Serial.println(F("Server connection failed")); 
+  }
   delay(1000);
 }
 
-void postDataToServer(unsigned int length) {
-  Serial.println(F("Connecting to server"));
-  if (client.connect(serverName, 49156)) {
-    Serial.println(F("Sending data"));
+void sendData(String thisData) {
+  // if there's a successful connection:
+    Serial.println(F("Data sending"));
+    // send the HTTP POST request:
     client.println(F("POST /api/sensors HTTP/1.1"));
-    client.println(F("HOST: 128.199.246.241:49156"));
+    client.println(F("Host: 128.199.246.241:49157"));
+    client.print(F("Content-Length: "));
+    client.println(thisData.length());
+
+    // last pieces of the HTTP PUT request:
     client.println(F("Content-Type: application/json"));
-    client.print(F("Content-Length: 45"));
-//    client.println(length);
-    client.println(F("Connection: close"));
-    Serial.println(F("Done"));
-  } 
+    client.println();
+
+    // here's the actual content of the PUT request:
+    client.println(thisData);
+    Serial.println(F("Data sent"));
 }
